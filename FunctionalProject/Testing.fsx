2cebdbdb0291d9a9b0405df414e081eb34079b71
toszi <toszi18@gmail.com>
2021-05-18 12:15:15 +0200

    .gitignore + use of generator had been made properly

--- FunctionalProject/Testing.fsx
+++ FunctionalProject/Testing.fsx
@@ -9,15 +9,12 @@ namespace Project
 
     module Testing =
         // Generates a random file id to be used. Needs to be added to the model & the api.
-        let fileIdGenerator() = Gen.choose(2, 4) |> Gen.sample 0 1 |> Seq.exactlyOne
+        let fileIdGenerator = Gen.choose(2, 4)
         let directoryIdGenerator() = Gen.choose(1, 21) |> Gen.sample 0 1 |> Seq.exactlyOne
         // Customer generator that creates files.
         let fileGenerator() = Arb.generate<Model.File>|> Gen.nonEmptyListOf |> Gen.sample 0 1 |> Seq.exactlyOne
         // Generates a random string.
         let stringGenerator() = Arb.generate<NonEmptyString>|> Gen.sample 100 1
-       
-        // Generates the random id to be used.
-        let fileId = fileIdGenerator()
         
         // API Operations.
         type API() =
@@ -28,16 +25,15 @@ namespace Project
         // Model Based Testing 
         let spec =
         // First command here.Test for getting a file and matching the id up against the model & the api.
-            let getFileMetaInformation = { new Command<API, string>() with
+            let getFileMetaInformation id = { new Command<API, string>() with
                         // Here we retrieve the id of the API file.
-                override __.RunActual api = api.fileMetaInformationById(0, fileIdGenerator()); api
+                override __.RunActual api = api.fileMetaInformationById(0, id); api
                         // Here we retrieve the id of the Model file.
-                override __.RunModel fileName = (Model.files.Head.Item (fileIdGenerator())).name
+                override __.RunModel fileName = (Model.files.Head.Item (id)).name
                         // Here we match the two id's to see if the states of both the api and the model are equal.
-                override __.Post(api, fileName ) = api.fileMetaInformationById(0, fileIdGenerator()).name = fileName  |@ sprintf "model: %s <> api: %s" fileName (api.fileMetaInformationById(0,fileIdGenerator()).name)
+                override __.Post(api, fileName ) = api.fileMetaInformationById(0, id).name = fileName  |@ sprintf "model: %s <> api: %s" fileName (api.fileMetaInformationById(0, id).name)
                         // Print actual and model.
                 override __.ToString() = "getFileMetaInformation"
- 
             }  
         // Check directory meta information.
              let getDirectoryMetaInformation = { new Command<API, string>() with
@@ -53,8 +49,11 @@ namespace Project
             { new ICommandGenerator<API,string> with
                 member __.InitialActual = API()
                 member __.InitialModel = ""
-                member __.Next model = Gen.elements [getFileMetaInformation; getDirectoryMetaInformation]}
-        
+                member __.Next model = 
+                        let fileMetaInformationGen = [Gen.map getFileMetaInformation fileIdGenerator] 
+                        Gen.oneof (fileMetaInformationGen)
+            }
+                
         //Check.One({Config.Verbose with MaxTest = 1;}, Command.toProperty spec);;
         Check.Quick(Command.toProperty spec)
 
