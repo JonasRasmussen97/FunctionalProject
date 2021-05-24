355e45605368c364992e4b805549077b680179a6
toszi <toszi18@gmail.com>
2021-05-24 10:43:29 +0200

    changed indenting and a single value

--- FunctionalProject/Utilities.fs
+++ FunctionalProject/Utilities.fs
@@ -8,11 +8,11 @@ open FunctionalProject.Model.Model
 module Utilities = 
 
     // Credits: https://stackoverflow.com/questions/2889961/f-insert-remove-item-from-list
-    let rec remove i l =
-        match i, l with
-            | 0, x::xs -> xs
-            | i, x::xs -> x::remove (i - 1) xs
-            | i, [] -> failwith "index out of range"
+    let rec remove index list =
+        match index, list with
+            | 0, head::restOfArr -> restOfArr
+            | index, head::restOfArr -> head::remove (index - 1) restOfArr
+            | index, [] -> failwith "index out of range"
 
     let getAllFileIds (list:API.FileMetaData list) = 
         list |> List.map (fun e -> e.id)
@@ -21,7 +21,7 @@ module Utilities =
     let createFileModel (model:InModel) dirId  userId name timeStamp = 
         let canCreateFile = if (userId <> 0) then false else true
         match canCreateFile with 
-            | true -> {Fail = None; Success = Some({model with files = {id=model.currentFileId; version=1; versionChanged=1; name="README.txt"; parentId=15; timestamp="637479675580000000"}::model.files;currentFileId = model.currentFileId+1})} 
+            | true -> {Fail = None; Success = Some({model with files = {id=model.currentFileId; version=1; versionChanged=1; name=name; parentId=dirId; timestamp=timeStamp}::model.files;currentFileId = model.currentFileId+1})} 
             | false -> {Fail = None; Success = None} // {model with currentFileId = model.currentFileId+1}
 
     let deleteFileModel (model: InModel) userId fileId = 
